9c1fc3b4417beb63a4103bfe00609fceb5c0a4ad
danielvargass97 <50029247+danielvargass97@users.noreply.github.com>
2020-04-14 21:26:04 -0500

    vista administrador avance

--- src/main/java/edu/eci/cvds/vista/PrincipalBean.java
+++ src/main/java/edu/eci/cvds/vista/PrincipalBean.java
@@ -14,14 +14,18 @@ import java.io.IOException;
 
 import edu.eci.cvds.servicios.IniciativaServicios;
 import edu.eci.cvds.servicios.ServiciosException;
+import edu.eci.cvds.entidades.Usuario;
 
+import java.util.List;
 
 @SuppressWarnings("deprecation")
 @ManagedBean(name = "principalBean")
 @SessionScoped
 public class PrincipalBean extends BasePageBean {
+
 	@Inject
     private IniciativaServicios iniciativaServicios;
+    private String nombre;
 
 	
 	public void iniciarSesion(String email,String contrasena) throws IOException, ServiciosException {
@@ -30,6 +34,7 @@ public class PrincipalBean extends BasePageBean {
 		if(iniciativaServicios.validarUsuario(email, contrasena)) {
 			HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(true);
 	           session.setAttribute("correo", email);
+	           nombre = iniciativaServicios.consultarUsuario(email).getNombre();
 			FacesContext.getCurrentInstance().getExternalContext().redirect("dos.xhtml");
 		}else {
 				FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
@@ -39,4 +44,11 @@ public class PrincipalBean extends BasePageBean {
 		
 	}
     
+    public String getNombre(){
+    	return nombre;
+    }
+
+    public List <Usuario> consultarUsuarios() throws ServiciosException{
+    	return iniciativaServicios.consultarUsuarios();
+    }
 }
