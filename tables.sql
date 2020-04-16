0a5f68f60b31c34c4f226164f5d6b8c8fa0a6a03
Esteban Bernal <estebanb0203@hotmail.com>
2020-04-15 19:48:59 -0500

    agregando area del proponente a Iniciativa

--- tables.sql
+++ tables.sql
@@ -16,13 +16,14 @@ usuarioproponente int NOT NULL,
 palabrasclave varchar(20) NOT NULL,
 fechaRegistro DATE NOT NULL,
 estado varchar(30) NOT NULL,
+areaProponente varchar(30) NOT NULL,
 PRIMARY KEY(num),
 FOREIGN KEY (usuarioproponente)
-REFERENCES USUARIO (id) 
+REFERENCES USUARIO (id),
+FOREIGN KEY (areaProponente) 
+REFERENCES USUARIO (tipoUsuario)
 );
 
 
 insert into USUARIO (id,nombre,apellido,email,telefono,tipoUsuario,contrasena) values (2158130,'Johann','Bogota','johann.bogota@mail.escuelaing.edu.co',123444,'administrador','1234');
 insert into USUARIO (id,nombre,apellido,email,telefono,tipoUsuario,contrasena) values (2157826,'Johan','Guerrero','johan.Guerrero@mail.escuelaing.edu.co',314021111,'usuario de consulta','0000');
-
-insert into INICIATIVA (num,descripcion,usuarioproponente,palabrasclave) values (0001,'descripcion iniciati',2157826,'palabras clave ini')	
